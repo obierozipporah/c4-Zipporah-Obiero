61f0ac9ace1d258e211ca24f5cae99de64c3829d
Zipporah Obiero <obierozipporah28@gmail.com>
2023-11-10 11:02:53 +0300

    react project
    first react project

---
+++ SRC/hello-world/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+      <h1>Hello World</h1>
+     
+        <p>
+           I am Zipporah Obiero, and this is my first React application
+        </p>
+  </div>
+     
+  )
+}
+
+export default App
